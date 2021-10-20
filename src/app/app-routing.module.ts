690652c20b812e88400cfea45bac59b41638aae5
cristian olivares <cristian.olivares.araya@gmail.com>
2021-10-20 16:07:59 -0300

    add new module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-//import { ConsumerCreditComponent } from "./consumer-credit/consumer-credit.component";
+import { ConsumerCreditComponent } from "./consumer-credit/consumer-credit.component";
 //import { MortgageCreditComponent } from './mortgage-credit/mortgage-credit.component';
 import { PagenotfoundComponent } from "./pagenotfound/pagenotfound.component";
 import { SelectCreditComponent } from "./select-credit/select-credit.component";
@@ -9,7 +9,7 @@ import { WelcomeComponent } from "./welcome/welcome.component";
 const routes: Routes = [
   { path: "", component: WelcomeComponent },
   { path: "select", component: SelectCreditComponent },
-  //{ path: "consumer", component: ConsumerCreditComponent },
+  { path: "consumer", component: ConsumerCreditComponent },
   //{path: 'mortgage', component : MortgageCreditComponent},
   { path: "**", component: PagenotfoundComponent },
 ];
